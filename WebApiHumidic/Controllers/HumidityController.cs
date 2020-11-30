bf15c00e6285a10b3b7b489eca26e80e58ab7378
Rania Zarka <rani0401@edu.easj.dk>
2020-11-30 11:10:19 +0100

    small change

--- WebApiHumidic/Controllers/HumidityController.cs
+++ WebApiHumidic/Controllers/HumidityController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApiHumidic.Model;
 
 namespace WebApiHumidic.Controllers
 {
